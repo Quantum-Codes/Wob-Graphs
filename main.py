25296a1ece0f8da74405f7e67a192d2ae7140be7
QuantumCodes <>
2023-02-20 16:15:20 +0000

    FIXED CROWDED X LABEL FINALLY

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests, json, datetime
 import matplotlib.pyplot as plt
-#from matplotlib.ticker import AutoMinorLocator
 plt.style.use(['dark_background'])
 
 
 def graph(user, type, multi=False):
   if multi:
     global lines
-  plt.figure()
+  fig = plt.figure()
   if multi:
     for item in lines[type]:
       plt.plot(item[0], item[1], "o--", linewidth=3)
@@ -16,7 +15,7 @@ def graph(user, type, multi=False):
   else:
     try:
       with open(f"stats/{user}.json", "r") as file:
-        data = json.loads(file.read())
+        data = json.load(file)
     except FileNotFoundError:
       return "no file"
     y = data[type]
@@ -27,22 +26,28 @@ def graph(user, type, multi=False):
     plt.plot(x, y, "o--", c = "yellow", linewidth=3)
     name = data['name']
     plt.title(f"{name}'s {type} graph")
+  
 
-  label_x = x.copy()
-  if len(x) > 25:
-    label_x[1::2] = len(label_x[1::2]) * [""] #alternate position blank. leaves the first item
   plt.grid(c="#7a7a7a")
   plt.xlabel('Timestamp')
   plt.ylabel(type.title())
-  #plt.ticklabel_format(style='sci')
-  plt.xticks(rotation=45, labels = label_x, ticks = x)
+  fig.canvas.draw() #to update x labels, stuff
+  axes = plt.gca()
+  label_x = axes.get_xticklabels()
+  #x = axes.get_ticks()
+  #print(x)
+  label_x = [item.get_text() for item in label_x]
+  if len(label_x) > 25:
+    label_x[1::2] = len(label_x[1::2]) * [""] #alternate position blank. leaves the first item
+    print(label_x)
+    axes.set_xticklabels(label_x, rotation=45)
+  #plt.xticks(rotation=45, labels = label_x, ticks = x)
   plt.tight_layout()
   plt.savefig(f"images/{user}-{type}.png", dpi=100)
   plt.close()
   if not multi:
     return (x, y)
 
-#print(tracklist)
 
 def graph_all():
   global lines
@@ -62,12 +67,15 @@ def graph_all():
     lines["following"].append(bc)
     lines["posts"].append(ac)
 
+
 #graph("60db0c5a956cdbbd0489eff6", "posts")
 #"""
 lines = {"following": [], "followers": [], "posts": []}
-graph_all()
-with open("stats/wasteof.json", "w") as file:
-  file.write(json.dumps(lines, indent=2))
+#graph_all()
+#with open("stats/wasteof.json", "w") as file:
+#  json.dump(lines, indent=2)
+with open("stats/wasteof.json", "r") as file:
+  lines = json.load(file)
 graph("Wasteof", "posts", True)
 graph("Wasteof", "followers", True)
 graph("Wasteof", "following", True)
