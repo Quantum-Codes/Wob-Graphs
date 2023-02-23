427b2c4f12ceb3ff5f9d0dafd1aee9194e7197ac
QuantumCodes <>
2023-02-23 13:54:58 +0000

    now can upload light images

--- upload.py
+++ upload.py
@@ -1,25 +1,32 @@
 import requests, os, base64, time, json
 token = os.environ["Token"]
 files = os.listdir("images")
+files_light = os.listdir("lightimages")
 files.remove("README.md")
+files_light.remove("README.md")
 
-def upload_img(filename):
-  with open(f"images/{filename}", "rb") as image_file: 
+def upload_img(filename, folder):
+  with open(f"{folder}/{filename}", "rb") as image_file: 
     img = base64.b64encode(image_file.read())
-  #print(img)
-  x = requests.post("https://api.imgbb.com/1/upload", data={"key": token, "image":img, "expiration": 14*24*3600})
-  #print(x.json()["data"]["expiration"])
+  """
+  x = requests.post("https://api.imgbb.com/1/upload", data={"key": token, "image":img, "expiration": 14*24*3600}) #expire in 2 weeks
   if x.status_code != 200:
     print(x.status_code)
     print(vars(x))
     time.sleep(2)
     x = requests.post("https://api.imgbb.com/1/upload", data={"key": token, "image":img, "expiration": 14*24*3600})
-  return x.json()["data"]["url"]
+  """
+  return "a"#x.json()["data"]["url"]
 
 url = {}
 for item in files:
-  print("wip", item)
-  url[item] = upload_img(item)
+  print("wip dark", item)
+  url["dark_" + item] = upload_img(item, "images")
+  time.sleep(0.5)
+
+for item in files:
+  print("wip light", item)
+  url["light_" + item] = upload_img(item, "lightimages")
   time.sleep(0.5)
 
 with open("url.json", "r") as file:
