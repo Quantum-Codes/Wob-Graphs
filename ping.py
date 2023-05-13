974bc678e9b48c982dba3d6a9f80cd5d1020306b
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2023-05-13 23:06:54 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -4,7 +4,7 @@ def ping():
   return requests.get("https://Wasteof-api-test.quantumcodes.repl.co", headers={ "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
 
 print("started")
-try:
+try: #DOESNT CATCH SSL
   ping()
   print("done")
 except Exception as e:
